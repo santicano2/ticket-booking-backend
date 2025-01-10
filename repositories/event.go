583cb58bb5a294bf9c22ac2e77d33f05ac8e1ed7
Santi <canosantiago404@gmail.com>
2025-01-10 13:14:48 -0300

    feat: added events handler and mock data

--- repositories/event.go
+++ repositories/event.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	"context"
+	"time"
 
 	"github.com/santicano2/ticket-booking/models"
 )
@@ -11,7 +12,18 @@ type EventRepository struct {
 }
 
 func (r *EventRepository) GetMany(ctx context.Context) ([]*models.Event, error) {
-	return nil, nil
+	events := []*models.Event{}
+
+	events = append(events, &models.Event{
+		ID: "135315315631",
+		Name: "Event 1",
+		Location: "Location 1",
+		Date: time.Now(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
+
+	return events, nil
 }
 
 func (r *EventRepository) GetOne(ctx context.Context, eventId string) (*models.Event, error) {
