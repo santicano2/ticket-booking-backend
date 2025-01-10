15603e0c310f7adc69b419906468b7d56c41b183
Santi <canosantiago404@gmail.com>
2025-01-10 15:52:08 -0300

    feat: db config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/santicano2/ticket-booking/config"
+	"github.com/santicano2/ticket-booking/db"
 	"github.com/santicano2/ticket-booking/handlers"
 	"github.com/santicano2/ticket-booking/repositories"
 )
 
 func main() {
+	envConfig := config.NewEnvConfig()
+	db := db.Init(envConfig, db.DBMigrator)
+
 	app := fiber.New(fiber.Config{
 		AppName: "TicketBookingAPI",
 		ServerHeader: "fiber",
 	})
 
 	// Repositories
-	eventRepository := repositories.NewEventRepository(nil)
+	eventRepository := repositories.NewEventRepository(db)
 
 	// Routing
 	server := app.Group("/api")
---
+++ db/auto-migrate.go
@@ -0,0 +1,10 @@
+package db
+
+import (
+	"github.com/santicano2/ticket-booking/models"
+	"gorm.io/gorm"
+)
+
+func DBMigrator(db *gorm.DB) error {
+	return db.AutoMigrate(&models.Event{})
+}
